4a84c91ad74db717a7ab0033ff4f90f1be8d4894
Unknown <mikulasek@live.com>
2018-11-16 00:41:18 +0100

    Plugging into kres #1

--- ratelim/rank.mk
+++
@@ -1,5 +0,0 @@
-whalebone_CFLAGS := -fvisibility=hidden -fPIC
-whalebone_SOURCES := modules/whalebone/whalebone.c
-whalebone_DEPEND := $(libkres)
-whalebone_LIBS := $(contrib_TARGET) $(libkres_TARGET) $(libkres_LIBS) 
-$(call make_c_module,whalebone)
---
+++ ratelim/ratelim.mk
@@ -0,0 +1,5 @@
+ratelim_CFLAGS := -fvisibility=hidden -fPIC
+ratelim_SOURCES := modules/ratelim/ratelim.c
+ratelim_DEPEND := $(libkres)
+ratelim_LIBS := $(contrib_TARGET) $(libkres_TARGET) $(libkres_LIBS) 
+$(call make_c_module,ratelim)
