a49feb1abb96f340a21c0a7cefecb97497cdde98
Unknown <mikulasek@live.com>
2018-11-16 01:36:23 +0100

    Plugging into kres #6

--- ratelim/ratelim.mk
+++ ratelim/ratelim.mk
@@ -1,5 +1,5 @@
 ratelim_CFLAGS := -fvisibility=hidden -fPIC
-ratelim_SOURCES := modules/ratelim/ratelim.c
+ratelim_SOURCES := modules/ratelim/ratelim.c modules/ratelim/main.c
 ratelim_DEPEND := $(libkres)
 ratelim_LIBS := $(contrib_TARGET) $(libkres_TARGET) $(libkres_LIBS) 
 $(call make_c_module,ratelim)
